46addb6f155695e3deffbae2eef36c6363e1c2d1
thatboy-echo <zhuguangfeng.thatboy@gmail.com>
2020-06-11 08:53:27 +0800

    增加Logger类

--- Logger/Logger.hpp
+++ Logger/Logger.hpp
@@ -7,16 +7,12 @@
 #include <sstream>
 #include <chrono>
 #include <iomanip>
+#include <mutex>
 
-namespace
+namespace __hide_namespace__
 {
-	namespace __hide_namespace__
+	namespace item
 	{
-		/// <summary>
-		/// File to write logs
-		/// </summary>
-		std::string logFileName{ "log.txt" };
-
 		/// <summary>
 		/// Mark the level of logger info
 		/// </summary>
@@ -34,10 +30,10 @@ namespace
 			" [ERROR] ",
 			" [FATAL] "
 		};
-		template <class _Elem, class _Traits>
-		std::basic_ostream<_Elem, _Traits>& operator<<(std::basic_ostream<_Elem, _Traits>& os, const LogLevel& level)
+
+		std::ostream& operator<<(std::ostream& logger, const LogLevel& level)
 		{
-			return os << constexprLogLeveString[static_cast<int>(level)];
+			return logger << constexprLogLeveString[static_cast<int>(level)];
 		}
 
 		/// <summary>
@@ -47,8 +43,8 @@ namespace
 		{
 			Time
 		};
-		template <class _Elem, class _Traits>
-		std::basic_ostream<_Elem, _Traits>& operator<<(std::basic_ostream<_Elem, _Traits>& os, const FunctionalInfo& func)
+
+		std::ostream& operator<<(std::ostream& logger, const FunctionalInfo& func)
 		{
 			switch (func)
 			{
@@ -57,119 +53,170 @@ namespace
 				tm tm;
 				time_t timeStamp = time(nullptr);
 				localtime_s(&tm, &timeStamp);
-				return os << tm.tm_year + 1900 << '-' << tm.tm_mon + 1 << '-' << tm.tm_mday << ' '
+				return logger << tm.tm_year + 1900 << '-' << tm.tm_mon + 1 << '-' << tm.tm_mday << ' '
 					<< std::setfill('0')
 					<< std::setw(2) << tm.tm_hour
 					<< ':'
 					<< std::setw(2) << tm.tm_min
 					<< ':'
-					<< std::setw(2) << tm.tm_sec << std::endl;
+					<< std::setw(2) << tm.tm_sec
+					<< ' ';
 			}
 			default:
 				break;
 			}
-			return os;
+			return logger;
+		}
+	}
+
+	struct Logger;
+	void _log_args_(std::ostream&);
+	template<typename T, typename ...Arg>
+	void _log_args_(std::ostream&, const T& t, const Arg&...);
+	template <typename ...Arg>
+	void log(Logger&, const Arg&...);
+
+	// Final
+	void _log_args_(std::ostream& logger)
+	{
+		logger << std::endl;
+	}
+	// Next
+	template<typename T, typename ...Arg>
+	void _log_args_(std::ostream& logger, const T& t, const Arg&... args)
+	{
+		_log_args_(logger << t, std::forward<const Arg&>(args)...);
+	}
+
+	/// <summary>
+	/// Logger
+	/// </summary>
+	struct Logger
+		: protected std::mutex
+	{
+	protected:
+		std::ostream& os;
+	public:
+		Logger(std::ostream& os_) :os(os_) {}
+
+		template <typename ...Arg>
+		friend void log(Logger&, const Arg&...);
+		template <typename ...Arg>
+		void log(const Arg&... args)
+		{
+			__hide_namespace__::log(*this, std::forward<const Arg&>(args)...);
+		}
+	};
+
+	struct FileLogger
+		: protected std::mutex
+	{
+	protected:
+		std::ofstream os;
+
+	public:
+		FileLogger() = default;
+		FileLogger(const char* file)
+			: os(file, std::ios::app | std::ios::out)
+		{}
+		void open(const char* _Filename, std::ios_base::open_mode _Mode)
+		{
+			return os.open(_Filename, std::ios_base::out | std::ios_base::app | _Mode);
+		}
+		void open(const wchar_t* _Filename, std::ios_base::open_mode _Mode)
+		{
+			return os.open(_Filename, std::ios_base::out | std::ios_base::app | _Mode);
+		}
+		void open(const unsigned short* _Filename, std::ios_base::open_mode _Mode)
+		{
+			return os.open(_Filename, std::ios_base::out | std::ios_base::app | _Mode);
+		}
+		void open(const char* _Filename, std::ios_base::openmode _Mode = std::ios_base::out | std::ios_base::app, int _Prot = std::ios_base::_Default_open_prot)
+		{
+			return os.open(_Filename, std::ios_base::out | std::ios_base::app | _Mode, _Prot);
+		}
+		void open(const wchar_t* _Filename, std::ios_base::openmode _Mode = std::ios_base::out | std::ios_base::app, int _Prot = std::ios_base::_Default_open_prot)
+		{
+			return os.open(_Filename, std::ios_base::out | std::ios_base::app | _Mode, _Prot);
+		}
+		void open(const unsigned short* _Filename, std::ios_base::openmode _Mode = std::ios_base::out | std::ios_base::app, int _Prot = std::ios_base::_Default_open_prot)
+		{
+			return os.open(_Filename, std::ios_base::out | std::ios_base::app | _Mode, _Prot);
+		}
+		void open(const std::string& _Str, std::ios_base::openmode _Mode = std::ios_base::out | std::ios_base::app, int _Prot = std::ios_base::_Default_open_prot)
+		{
+			return os.open(_Str, std::ios_base::out | std::ios_base::app | _Mode, _Prot);
+		}
+		void open(const std::wstring& _Str, std::ios_base::openmode _Mode = std::ios_base::out | std::ios_base::app, int _Prot = std::ios_base::_Default_open_prot)
+		{
+			return os.open(_Str, std::ios_base::out | std::ios_base::app | _Mode, _Prot);
 		}
 
-		template<class _Elem, class _Traits, typename ...Arg>
-		void log(std::basic_ostream<_Elem, _Traits>& os, const Arg&... args);
-		template<class _Elem, class _Traits, typename ...Arg>
-		void log(std::basic_ofstream<_Elem, _Traits>& os, const Arg&... args);
-		template<class _Elem, class _Traits, typename ...Arg>
-		void log(std::basic_ostringstream<_Elem, _Traits>& os, const Arg&... args);
-		template<class _Elem, class _Traits, typename ...Arg>
-		void log(std::basic_iostream<_Elem, _Traits>& os, const Arg&... args);
-		template<class _Elem, class _Traits, typename ...Arg>
-		void log(std::basic_fstream<_Elem, _Traits>& os, const Arg&... args);
-		template<class _Elem, class _Traits, typename ...Arg>
-		void log(std::basic_stringstream<_Elem, _Traits>& os, const Arg&... args);
-		template<typename T, typename ...Arg>
-		void log(const T& t, const Arg&... args);
-
-		template <class _Elem, class _Traits>
-		void _log_args_(std::basic_ostream<_Elem, _Traits>& os);
-		template<class _Elem, class _Traits, typename T, typename ...Arg>
-		void _log_args_(std::basic_ostream<_Elem, _Traits>& os, const T& t, const Arg&... args);
-
-
-		// Final
-		template <class _Elem, class _Traits>
-		void _log_args_(std::basic_ostream<_Elem, _Traits>& os)
+		bool is_open() const
 		{
-			os << std::endl;
+			return os.is_open();
 		}
-		// Next
-		template<class _Elem, class _Traits, typename T, typename ...Arg>
-		void _log_args_(std::basic_ostream<_Elem, _Traits>& os, const T& t, const Arg&... args)
+		operator bool() const
 		{
-			_log_args_(os << t, std::forward<const Arg&>(args)...);
+			return os.operator bool();
+		}
+		bool operator!() const
+		{
+			return !os;
+		}
+		bool fail() const
+		{
+			return os.fail();
+		}
+		bool good() const
+		{
+			return os.good();
+		}
+		bool bad() const
+		{
+			return os.bad();
+		}
+		void close()
+		{
+			return os.close();
 		}
 
-		/// <summary>
-		/// Log > basic_ostream
-		/// </summary>
-		/// <param name="t">Args to write</param>
-		template<class _Elem, class _Traits, typename ...Arg>
-		void log(std::basic_ostream<_Elem, _Traits>& os, const Arg&... args)
+		template <typename ...Arg>
+		friend void log(FileLogger&, const Arg&...);
+		template <typename ...Arg>
+		friend void log(FileLogger&&, const Arg&...);
+		template <typename ...Arg>
+		void log(const Arg&... args)
 		{
-			_log_args_(os, FunctionalInfo::Time, std::forward<const Arg&>(args)...);
+			__hide_namespace__::log(*this, std::forward<const Arg&>(args)...);
 		}
-		/// <summary>
-		/// Log > basic_ofstream
-		/// </summary>
-		/// <param name="t">Args to write</param>
-		template<class _Elem, class _Traits, typename ...Arg>
-		void log(std::basic_ofstream<_Elem, _Traits>& os, const Arg&... args)
-		{
-			_log_args_(os, FunctionalInfo::Time, std::forward<const Arg&>(args)...);
-		}
-		/// <summary>
-		/// Log > basic_ostringstream
-		/// </summary>
-		/// <param name="t">Args to write</param>
-		template<class _Elem, class _Traits, typename ...Arg>
-		void log(std::basic_ostringstream<_Elem, _Traits>& os, const Arg&... args)
-		{
-			_log_args_(os, FunctionalInfo::Time, std::forward<const Arg&>(args)...);
-		}
-		/// <summary>
-		/// Log > basic_iostream
-		/// </summary>
-		/// <param name="t">Args to write</param>
-		template<class _Elem, class _Traits, typename ...Arg>
-		void log(std::basic_iostream<_Elem, _Traits>& os, const Arg&... args)
-		{
-			_log_args_(os, FunctionalInfo::Time,std::forward<const Arg&>(args)...);
-		}
-		/// <summary>
-		/// Log > basic_fstream
-		/// </summary>
-		/// <param name="t">Args to write</param>
-		template<class _Elem, class _Traits, typename ...Arg>
-		void log(std::basic_fstream<_Elem, _Traits>& os, const Arg&... args)
-		{
-			_log_args_(os, FunctionalInfo::Time, std::forward<const Arg&>(args)...);
-		}
-		/// <summary>
-		/// Log > basic_stringstream
-		/// </summary>
-		/// <param name="t">Args to write</param>
-		template<class _Elem, class _Traits, typename ...Arg>
-		void log(std::basic_stringstream<_Elem, _Traits>& os, const Arg&... args)
-		{
-			_log_args_(os, FunctionalInfo::Time, std::forward<const Arg&>(args)...);
-		}
-		/// <summary>
-		/// Log > file and clog
-		/// </summary>
-		/// <param name="t">Args to write</param>
-		template<typename T, typename ...Arg>
-		void log(const T& t, const Arg&... args)
-		{
-			std::ofstream os(logFileName, std::ios::app);
-			log(os, t, std::forward<const Arg&>(args)...);
-			log(std::clog, t, std::forward<const Arg&>(args)...);
-		}
+	};
+
+	/// <summary>
+	/// Log
+	/// </summary>
+	template <typename ...Arg>
+	void log(Logger& logger, const Arg&... args)
+	{
+		std::lock_guard<std::mutex> lock(logger);
+		_log_args_(logger.os, item::FunctionalInfo::Time, std::forward<const Arg&>(args)...);
+	}
+
+	template<typename ...Arg>
+	void log(FileLogger& logger, const Arg& ...args)
+	{
+		std::lock_guard<std::mutex> lock(logger);
+		_log_args_(logger.os, item::FunctionalInfo::Time, std::forward<const Arg&>(args)...);
+	}
+
+	 /// <summary>
+	 /// Log
+	 /// </summary>
+	template <typename ...Arg>
+	void log(FileLogger&& logger, const Arg&... args)
+	{
+		std::lock_guard<std::mutex> lock(logger);
+		_log_args_(logger.os, item::FunctionalInfo::Time, std::forward<const Arg&>(args)...);
 	}
 }
 
@@ -183,10 +230,11 @@ namespace thatboy
 	/// </summary>
 	namespace logger
 	{
-		using __hide_namespace__::logFileName;
 		using __hide_namespace__::log;
-		using __hide_namespace__::LogLevel;
-		using __hide_namespace__::FunctionalInfo;
+		using __hide_namespace__::item::FunctionalInfo;
+		using __hide_namespace__::item::LogLevel;
+		using __hide_namespace__::Logger;
+		using __hide_namespace__::FileLogger;
 	}
 }
 
--- Logger/main.cpp
+++ Logger/main.cpp
@@ -2,7 +2,9 @@
 
 int main()
 {
-	thatboy::logger::log("hello world ", "你好，世界 ");
-	thatboy::logger::log(std::clog, "hello world ", "你好，世界 ");
+	thatboy::logger::FileLogger logger("log.txt");
+	logger.log("hello world ", "你好，世界 ");
+	thatboy::logger::log(logger, "测试Log &", 233);
+	thatboy::logger::log(thatboy::logger::FileLogger("log.txt"), "测试Log &&", 233);
 	return 0;
 }
