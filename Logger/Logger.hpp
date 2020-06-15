1f23b0ef32d5601527ab0ea837d38d5ec5a47618
thatboy-echo <zhuguangfeng.thatboy@gmail.com>
2020-06-15 17:20:01 +0800

    Update Logger.hpp

--- Logger/Logger.hpp
+++ Logger/Logger.hpp
@@ -8,6 +8,7 @@
 #include <chrono>
 #include <iomanip>
 #include <mutex>
+#include <codecvt> 
 
 namespace {
 	namespace __hide_namespace__
@@ -67,6 +68,77 @@ namespace {
 					break;
 				}
 				return logger;
+			}
+		}
+
+		namespace code_cast
+		{
+			std::string UnicodeToUTF8(const std::wstring& wstr)
+			{
+				std::string ret;
+				try {
+					std::wstring_convert< std::codecvt_utf8<wchar_t> > wcv;
+					ret = wcv.to_bytes(wstr);
+				}
+				catch (const std::exception& e) {
+					std::cerr << e.what() << std::endl;
+				}
+				return ret;
+			}
+
+			std::wstring UTF8ToUnicode(const std::string& str)
+			{
+				std::wstring ret;
+				try {
+					std::wstring_convert< std::codecvt_utf8<wchar_t> > wcv;
+					ret = wcv.from_bytes(str);
+				}
+				catch (const std::exception& e) {
+					std::cerr << e.what() << std::endl;
+				}
+				return ret;
+			}
+
+			std::string UnicodeToANSI(const std::wstring& wstr)
+			{
+				std::string ret;
+				std::mbstate_t state = {};
+				const wchar_t* src = wstr.data();
+				size_t len = std::wcsrtombs(nullptr, &src, 0, &state);
+				if (static_cast<size_t>(-1) != len) {
+					std::unique_ptr< char[] > buff(new char[len + 1]);
+					len = std::wcsrtombs(buff.get(), &src, len, &state);
+					if (static_cast<size_t>(-1) != len) {
+						ret.assign(buff.get(), len);
+					}
+				}
+				return ret;
+			}
+
+			std::wstring ANSIToUnicode(const std::string& str)
+			{
+				std::wstring ret;
+				std::mbstate_t state = {};
+				const char* src = str.data();
+				size_t len = std::mbsrtowcs(nullptr, &src, 0, &state);
+				if (static_cast<size_t>(-1) != len) {
+					std::unique_ptr< wchar_t[] > buff(new wchar_t[len + 1]);
+					len = std::mbsrtowcs(buff.get(), &src, len, &state);
+					if (static_cast<size_t>(-1) != len) {
+						ret.assign(buff.get(), len);
+					}
+				}
+				return ret;
+			}
+
+			std::string UTF8ToANSI(const std::string& str)
+			{
+				return UnicodeToANSI(UTF8ToUnicode(str));
+			}
+
+			std::string ANSIToUTF8(const std::string& str)
+			{
+				return UnicodeToUTF8(ANSIToUnicode(str));
 			}
 		}
 
@@ -277,6 +349,7 @@ namespace thatboy
 		using __hide_namespace__::item::LogLevel;
 		using __hide_namespace__::Logger;
 		using __hide_namespace__::FileLogger;
+		using namespace __hide_namespace__::code_cast;
 	}
 }
 
